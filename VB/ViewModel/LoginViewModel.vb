7228f4a140ae390ef2329117b6b3ea01c3400bf5
DevExpressExampleBot <fakeEmail>
2022-12-17 14:47:01 +0400

    Source auto update [skip ci]

--- VB/ViewModel/LoginViewModel.vb
+++ VB/ViewModel/LoginViewModel.vb
@@ -1,20 +1,25 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.DataAnnotations
 Imports DevExpress.Mvvm.POCO
 
 Namespace Example.ViewModel
-    <POCOViewModel> _
+
+    <POCOViewModel>
     Public Class LoginViewModel
+
         Public Shared Function Create() As LoginViewModel
             Return ViewModelSource.Create(Function() New LoginViewModel())
         End Function
+
         Protected Sub New()
         End Sub
 
-        Public Overridable Property UserName() As String
+        Public Overridable Property UserName As String
+
         Public Sub Login()
-            Me.GetService(Of IMessageBoxService)().Show("Login succeeded", "Login", MessageButton.OK, MessageIcon.Information, MessageResult.OK)
+            GetService(Of IMessageBoxService).Show("Login succeeded", "Login", MessageButton.OK, MessageIcon.Information, MessageResult.OK)
         End Sub
+
         Public Function CanLogin() As Boolean
             Return Not String.IsNullOrEmpty(UserName)
         End Function
